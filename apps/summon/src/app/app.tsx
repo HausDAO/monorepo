e46ba67205e771c78f639af0b7f07ee2de412ffa
jordan <jordan.lesich@gmail.com>
2022-11-02 12:48:32 -0700

    scaffold summon

---
+++ apps/summon/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="summon" />
+      <div />
+    </>
+  );
+}
+
+export default App;
--- apps/summon/src/main.tsx
+++ apps/summon/src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+
 import App from './app/app';
-import { HausThemeProvider } from '@daohaus/ui';
-import { HausConnectProvider } from '@daohaus/connect';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <HausThemeProvider>
-      <HausConnectProvider>
-        <App />
-      </HausConnectProvider>
-    </HausThemeProvider>
+    <App />
   </StrictMode>
 );

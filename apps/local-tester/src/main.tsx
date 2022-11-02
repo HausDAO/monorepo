e36d48dde44cd9aa2ecb78a6e33da17ed820069a
jordan <jordan.lesich@gmail.com>
2022-11-02 09:03:41 -0700

    setup app

---
+++ apps/local-tester/src/main.tsx
@@ -0,0 +1,19 @@
+import { HausConnectProvider } from '@daohaus/connect';
+import { HausThemeProvider } from '@daohaus/ui';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <HausThemeProvider>
+      <HausConnectProvider>
+        <App />
+      </HausConnectProvider>
+    </HausThemeProvider>
+  </React.StrictMode>
+);

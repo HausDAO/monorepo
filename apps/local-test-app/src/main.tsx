3aa0e15c4dde393cc08894120c6ca41f67a30cc9
jordan <jordan.lesich@gmail.com>
2022-11-18 09:24:32 -0800

    test external UI

--- apps/local-test-app/src/main.tsx
+++ apps/local-test-app/src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-import { HausThemeProvider } from '@daohaus/ui';
-import { DHConnectProvider } from '@daohaus/connect';
+
+import { ConnectProvider } from '@daohaus/connect-context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <HausThemeProvider>
-      <DHConnectProvider>
-        <App />
-      </DHConnectProvider>
-    </HausThemeProvider>
+    <ConnectProvider>
+      <App />
+    </ConnectProvider>
   </StrictMode>
 );

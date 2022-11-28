7b9efe466ebe6cf47bb895cff34efc55b401e623
skuhlmann <samkuhlmann@gmail.com>
2022-11-28 13:15:38 -0700

    fixes extra render issues

--- apps/admin/src/pages/DaoContainer.tsx
+++ apps/admin/src/pages/DaoContainer.tsx
@@ -2,17 +2,22 @@ import { useParams } from 'react-router-dom';
 import { useDHConnect } from '@daohaus/connect';
 import { MolochV3ContextProvider } from '@daohaus/moloch-v3-context';
 import Dao from './Dao';
+import { useMemo } from 'react';
 
 export function DaoContainer() {
   const { daochain, daoid } = useParams();
   const { address } = useDHConnect();
+
+  const graphApiKeys = useMemo(() => {
+    return { '0x1': process.env['NX_GRAPH_API_KEY_MAINNET'] };
+  }, []);
 
   return (
     <MolochV3ContextProvider
       address={address}
       daoid={daoid}
       daochain={daochain}
-      graphApiKeys={{ '0x1': process.env['NX_GRAPH_API_KEY_MAINNET'] }}
+      graphApiKeys={graphApiKeys}
     >
       <Dao />
     </MolochV3ContextProvider>

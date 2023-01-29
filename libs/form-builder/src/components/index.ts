068a9a7416d5660095a253a7ce2f2a191ae9e947
jordan <jordan.lesich@gmail.com>
2023-01-29 13:16:08 -0800

    disconnect formbuilder and formfactory

--- libs/form-builder/src/components/index.ts
+++ libs/form-builder/src/components/index.ts
@@ -1,7 +1,6 @@
-export * from './FormBuilderFactory';
 export * from './CoreFieldLookup';
-
 export * from './FieldSpacer';
 export * from './ToWeiInput';
 export * from './formFooter';
 export { useFormBuilder } from '@daohaus/form-builder-base';
+export { FormBuilderFactory } from '@daohaus/form-builder-base';

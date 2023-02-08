527f20ccb77cd0c38ca143d029dbcf64a6996c12
jordan <jordan.lesich@gmail.com>
2023-02-07 18:48:15 -0800

    query for safes

--- apps/admin-new/src/pages/Safes.tsx
+++ apps/admin-new/src/pages/Safes.tsx
@@ -1,5 +1,28 @@
+import { ValidNetwork } from '@daohaus/keychain-utils';
+import { useCurrentDao, useDaoData } from '@daohaus/moloch-v3-hooks';
+import { Link, ParLg, SingleColumnLayout } from '@daohaus/ui';
+import { generateGnosisUiLink } from '@daohaus/utils';
 import React from 'react';
+import { JSONDisplay } from '../components/JSONDisplay';
 
 export const Safes = () => {
-  return <div>Safes</div>;
+  const { daoChain } = useCurrentDao();
+  const { dao } = useDaoData();
+
+  return (
+    <SingleColumnLayout>
+      <ParLg>Treasury</ParLg>
+      <Link
+        linkType="external"
+        href={generateGnosisUiLink({
+          chainId: daoChain as ValidNetwork,
+          address: dao?.safeAddress,
+        })}
+      >
+        {dao?.safeAddress}
+      </Link>
+      <ParLg>Safes</ParLg>
+      <JSONDisplay data={dao?.vaults} />
+    </SingleColumnLayout>
+  );
 };

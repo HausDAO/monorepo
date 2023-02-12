d877320d0d45cc99f3f52946bbfe81e05e371290
Rowdy <mroddy5280@gmail.com>
2023-02-12 14:45:07 -0700

    Deleted Loading button after moving functionality into Button

--- libs/ui/src/components/atoms/Button/Button.stories.tsx
+++ libs/ui/src/components/atoms/Button/Button.stories.tsx
@@ -57,31 +57,56 @@ BaseButton.args = {
   isLoading: false,
   disabled: false,
   fullWidth: false,
-  justify: 'flex-start',
+  justify: 'center',
 };
 
 export const PrimaryButton = Template.bind({});
 PrimaryButton.args = {
   children: 'Button',
-  href: 'https://daohaus.club',
+  color: 'primary',
+  variant: 'solid',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
 };
 
 export const PrimaryButtonOutline = Template.bind({});
 PrimaryButtonOutline.args = {
   children: 'Button',
+  color: 'primary',
   variant: 'outline',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
 };
 
 export const PrimaryButtonAsLink = Template.bind({});
 PrimaryButtonAsLink.args = {
   children: 'Button',
+  color: 'primary',
   variant: 'link',
+  href: 'https://daohaus.club',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
 };
 
 export const SecondaryButton = Template.bind({});
 SecondaryButton.args = {
   children: 'Button',
   color: 'secondary',
+  variant: 'solid',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
 };
 
 export const SecondaryButtonOutline = Template.bind({});
@@ -89,6 +114,11 @@ SecondaryButtonOutline.args = {
   children: 'Button',
   color: 'secondary',
   variant: 'outline',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
 };
 
 export const SecondaryButtonAsLink = Template.bind({});
@@ -96,26 +126,51 @@ SecondaryButtonAsLink.args = {
   children: 'Button',
   color: 'secondary',
   variant: 'link',
+  href: 'https://daohaus.club',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
 };
 
 export const ButtonLeftIcon = Template.bind({});
 ButtonLeftIcon.args = {
   children: 'Button',
+  color: 'primary',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
   IconLeft: RiAlertLine,
-  href: 'https://daohaus.club',
 };
 
 export const ButtonRightIcon = Template.bind({});
 ButtonRightIcon.args = {
   children: 'Button',
+  color: 'primary',
+  variant: 'link',
+  href: 'https://daohaus.club',
+  size: 'md',
   isLoading: false,
   disabled: false,
+  fullWidth: false,
+  justify: 'center',
   IconRight: RiArrowDropDownLine,
 };
 
 export const ButtonWithLeftAndRightIcons = Template.bind({});
 ButtonWithLeftAndRightIcons.args = {
   children: 'Button',
+  color: 'primary',
+  variant: 'link',
+  href: 'https://daohaus.club',
+  size: 'md',
+  isLoading: false,
+  disabled: false,
+  fullWidth: false,
+  justify: 'center',
   IconLeft: RiAlertLine,
   IconRight: RiArrowDropDownLine,
 };

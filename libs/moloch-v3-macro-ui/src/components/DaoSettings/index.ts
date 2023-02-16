9a2e46736db76b0f3608963a4e0bc15aab3ff347
skuhlmann <samkuhlmann@gmail.com>
2023-02-16 11:44:55 -0700

    more exports

--- libs/moloch-v3-macro-ui/src/components/DaoSettings/index.ts
+++ libs/moloch-v3-macro-ui/src/components/DaoSettings/index.ts
@@ -1,1 +1,6 @@
+export * from './ContractSettings';
 export * from './DaoSettings';
+export * from './GovernanceSettings';
+export * from './MetadataSettings';
+export * from './ShamanSettings';
+export * from './ShamanList';

70a7c85d062a9d5298d38ed7fbc81dbb214fecfc
Rowdy <mroddy5280@gmail.com>
2023-02-23 12:23:53 -0700

    Commeneted out the story that was adding react router to the package

--- libs/ui/src/components/organisms/MemberCard/MemberCard.stories.tsx
+++ libs/ui/src/components/organisms/MemberCard/MemberCard.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Link as RouterLink, BrowserRouter as Router } from 'react-router-dom';
 import styled from 'styled-components';
 import { withRouter } from 'storybook-addon-react-router-v6';
 
@@ -82,38 +81,39 @@ MemberCardWithEns.args = {
   ],
 };
 
-export const MemberCardWithRouterLink = Template.bind({});
+// Commented out because this story was causing react-router to be added to the package
+// export const MemberCardWithRouterLink = Template.bind({});
 
-const StyledRouterLink = styled(RouterLink)`
-  ${DropdownLinkStyles}
-`;
+// const StyledRouterLink = styled(RouterLink)`
+//   ${DropdownLinkStyles}
+// `;
 
-MemberCardWithRouterLink.story = {
-  parameters: {
-    reactRouter: {
-      routePath: '/profile/:profileId',
-      routeParams: { profileId: '42' },
-      searchParams: { tab: 'activityLog' },
-      routeState: { fromPage: 'homePage' },
-    },
-  },
-};
+// MemberCardWithRouterLink.story = {
+//   parameters: {
+//     reactRouter: {
+//       routePath: '/profile/:profileId',
+//       routeParams: { profileId: '42' },
+//       searchParams: { tab: 'activityLog' },
+//       routeState: { fromPage: 'homePage' },
+//     },
+//   },
+// };
 
-MemberCardWithRouterLink.args = {
-  profile: sampleEnsProfile,
-  children: [
-    <MemberCardExplorerLink
-      key={'00001'}
-      profileAddress={sampleProfile.address}
-      explorerNetworkId="0x1"
-    >
-      View on Etherscan
-    </MemberCardExplorerLink>,
-    <MemberCardCopyAddress key="00002" profileAddress={sampleProfile.address}>
-      Copy Me!
-    </MemberCardCopyAddress>,
-    <MemberCardItem key={'00003'}>
-      <StyledRouterLink to="/profile">View Profile</StyledRouterLink>
-    </MemberCardItem>,
-  ],
-};
+// MemberCardWithRouterLink.args = {
+//   profile: sampleEnsProfile,
+//   children: [
+//     <MemberCardExplorerLink
+//       key={'00001'}
+//       profileAddress={sampleProfile.address}
+//       explorerNetworkId="0x1"
+//     >
+//       View on Etherscan
+//     </MemberCardExplorerLink>,
+//     <MemberCardCopyAddress key="00002" profileAddress={sampleProfile.address}>
+//       Copy Me!
+//     </MemberCardCopyAddress>,
+//     <MemberCardItem key={'00003'}>
+//       <StyledRouterLink to="/profile">View Profile</StyledRouterLink>
+//     </MemberCardItem>,
+//   ],
+// };

3eaa341107620353d2b061d03dcfbd697ee82843
jordan <jordan.lesich@gmail.com>
2023-02-28 12:17:12 -0800

    clean

--- apps/admin-new/src/pages/FormTest.tsx
+++ apps/admin-new/src/pages/FormTest.tsx
@@ -1,14 +1,13 @@
 import { FormBuilder } from '@daohaus/form-builder';
 import { useCurrentDao } from '@daohaus/moloch-v3-hooks';
-import React from 'react';
-import { PROPOSAL_FORMS } from '@daohaus/moloch-v3-legos';
+import { COMMON_FORMS } from '@daohaus/moloch-v3-legos';
 import { MolochFields } from '@daohaus/moloch-v3-fields';
 
 export const FormTest = () => {
   const { daoChain } = useCurrentDao();
   return (
     <FormBuilder
-      form={PROPOSAL_FORMS.GUILDKICK}
+      form={COMMON_FORMS.ADD_SAFE}
       targetNetwork={daoChain}
       customFields={MolochFields}
     />

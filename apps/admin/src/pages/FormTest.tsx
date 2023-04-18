c348c86ebc553be26691f4679c0697e76492a8cb
skuhlmann <samkuhlmann@gmail.com>
2023-04-18 12:56:50 -0600

    quick change

--- apps/admin/src/pages/FormTest.tsx
+++ apps/admin/src/pages/FormTest.tsx
@@ -7,7 +7,7 @@ export function FormTest() {
   const { daochain } = useParams();
   return (
     <FormBuilder
-      form={PROPOSAL_FORMS.SIGNAL}
+      form={PROPOSAL_FORMS.ISSUE}
       customFields={CustomFields}
       targetNetwork={daochain}
     />

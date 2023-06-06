4dd33951f1d6dab4510d7e4bf085c6f776cfc86b
skuhlmann <samkuhlmann@gmail.com>
2023-06-06 06:59:53 -0600

    moves some additional details proposal fields around

--- libs/moloch-v3-macro-ui/src/components/ProposalActionData/ProposalActionData.tsx
+++ libs/moloch-v3-macro-ui/src/components/ProposalActionData/ProposalActionData.tsx
@@ -81,103 +81,100 @@ export const ProposalActionData = ({
         <TitleContainer>
           <ParMd>Transaction Call Data</ParMd>
 
-          {open && (
+          {!actionData && (
+            <LoadingContainer>
+              <Spinner size="2rem" />
+            </LoadingContainer>
+          )}
+
+          {actionData && open && (
             <div onClick={handleToggle}>
               <StyledUpArrow />
             </div>
           )}
-          {!open && (
+          {actionData && !open && (
             <div onClick={handleToggle}>
               <StyledDownArrow />
             </div>
           )}
         </TitleContainer>
-        {open && (
-          <>
-            {!actionData && (
-              <LoadingContainer>
-                <Spinner size="6rem" />
-              </LoadingContainer>
-            )}
-
-            {actionData?.map((action, index) => {
-              if (isActionError(action)) {
-                return (
-                  <div
-                    className="display-segment data"
-                    key={`${action.message}-${index}`}
-                  >
-                    <H4 className="space">Action {index + 1}: Error</H4>
-                    <DataSm className="space">{action.message}</DataSm>
-                    <Divider className="space" />
-                    <DataSm className="space">
-                      <Bold>HEX DATA:</Bold>
-                    </DataSm>
-                    <DataSm className="space">{action.data}</DataSm>
-                  </div>
-                );
-              }
+        {open &&
+          actionData?.map((action, index) => {
+            if (isActionError(action)) {
               return (
-                <div className="display-segment" key={`action_${index}`}>
-                  <div className="data">
-                    <H4 className="space">
-                      Action {index + 1}: {action.name}
-                    </H4>
-                    <ActionAlert
-                      action={action}
-                      daoId={daoId}
-                      daoChain={daoChain}
-                      proposalType={proposal.proposalType}
-                      proposalActionConfig={proposalActionConfig}
-                    />
-                    <DataSm className="space">
-                      <Bold>TARGET</Bold>
-                    </DataSm>
-                    <AddressDisplay
-                      className="space"
-                      address={action.to}
-                      copy
-                      explorerNetworkId={network}
-                      truncate={isMobile}
-                    />
-                    <DataSm className="space">
-                      <Bold>VALUE</Bold>
-                    </DataSm>
-                    <DataSm className="space">{action.value}</DataSm>
-                    <Divider className="spaced-divider" />
-                  </div>
-                  {action.params?.map((arg, index) => {
-                    return (
-                      <div className="data" key={`${arg.name}-${index}`}>
-                        <DataSm className="space">
-                          <Bold>
-                            PARAM
-                            {index + 1}:{' '}
-                          </Bold>
-                          {arg.name}
-                        </DataSm>
-                        <DataSm className="space">
-                          <Bold>TYPE: </Bold>
-                          {arg.type}
-                        </DataSm>
-                        <DataSm className="space">
-                          <Bold>VALUE: </Bold>
-                        </DataSm>
-                        <ValueDisplay
-                          argValue={arg.value}
-                          argType={arg.type}
-                          network={network}
-                          isMobile={isMobile}
-                        />
-                        <Divider />
-                      </div>
-                    );
-                  })}
+                <div
+                  className="display-segment data"
+                  key={`${action.message}-${index}`}
+                >
+                  <H4 className="space">Action {index + 1}: Error</H4>
+                  <DataSm className="space">{action.message}</DataSm>
+                  <Divider className="space" />
+                  <DataSm className="space">
+                    <Bold>HEX DATA:</Bold>
+                  </DataSm>
+                  <DataSm className="space">{action.data}</DataSm>
                 </div>
               );
-            })}
-          </>
-        )}
+            }
+            return (
+              <div className="display-segment" key={`action_${index}`}>
+                <div className="data">
+                  <H4 className="space">
+                    Action {index + 1}: {action.name}
+                  </H4>
+                  <ActionAlert
+                    action={action}
+                    daoId={daoId}
+                    daoChain={daoChain}
+                    proposalType={proposal.proposalType}
+                    proposalActionConfig={proposalActionConfig}
+                  />
+                  <DataSm className="space">
+                    <Bold>TARGET</Bold>
+                  </DataSm>
+                  <AddressDisplay
+                    className="space"
+                    address={action.to}
+                    copy
+                    explorerNetworkId={network}
+                    truncate={isMobile}
+                  />
+                  <DataSm className="space">
+                    <Bold>VALUE</Bold>
+                  </DataSm>
+                  <DataSm className="space">{action.value}</DataSm>
+                  <Divider className="spaced-divider" />
+                </div>
+                {action.params?.map((arg, index) => {
+                  return (
+                    <div className="data" key={`${arg.name}-${index}`}>
+                      <DataSm className="space">
+                        <Bold>
+                          PARAM
+                          {index + 1}:{' '}
+                        </Bold>
+                        {arg.name}
+                      </DataSm>
+                      <DataSm className="space">
+                        <Bold>TYPE: </Bold>
+                        {arg.type}
+                      </DataSm>
+                      <DataSm className="space">
+                        <Bold>VALUE: </Bold>
+                      </DataSm>
+                      <ValueDisplay
+                        argValue={arg.value}
+                        argType={arg.type}
+                        network={network}
+                        isMobile={isMobile}
+                      />
+                      <Divider />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
       </DisplayContainer>
       {decodeError && (
         <ProposalWarning

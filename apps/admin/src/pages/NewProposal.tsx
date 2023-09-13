51cee955c304d9cb988e8d7082a2ad5e52250ffb
skuhlmann <samkuhlmann@gmail.com>
2023-09-13 11:34:31 -0600

    updates prop query

--- apps/admin/src/pages/NewProposal.tsx
+++ apps/admin/src/pages/NewProposal.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { FormBuilder } from '@daohaus/form-builder';
 import {

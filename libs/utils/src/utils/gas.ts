d4a216150dd583e6e9784e81c62b930646e0d6f0
dekanbro <dekanbrown@odyssy.io>
2023-10-05 08:47:55 -0600

    change gas multipler to 5, move constant to file and update multicall and processing

--- libs/utils/src/utils/gas.ts
+++ libs/utils/src/utils/gas.ts
@@ -1,10 +1,8 @@
 import { ValidNetwork } from '@daohaus/keychain-utils';
 import { weiUnits, formatUnits, parseGwei } from 'viem';
 import { createViemClient } from './viem';
+import { GAS_BUFFER_MULTIPLIER, L2_ADDITIONAL_GAS, PROCESS_PROPOSAL_GAS_LIMIT_ADDITION } from '../constants';
 
-// Adding to the gas limit to account for cost of processProposal
-export const PROCESS_PROPOSAL_GAS_LIMIT_ADDITION = 150000;
-export const L2_ADDITIONAL_GAS = 5000000;
 
 // OPTIMISM challenge - how to display this estimated gas fee like we do on other networks.
 // ethers.js getFeeData returns maxFeePerGas as undefined,  might need to use gasPrice some how, then fetch the L1 amount
@@ -32,13 +30,13 @@ export const getProcessingGasLimit = (
         ? Number(actionGasEstimate) +
             PROCESS_PROPOSAL_GAS_LIMIT_ADDITION +
             L2_ADDITIONAL_GAS
-        : PROCESS_PROPOSAL_GAS_LIMIT_ADDITION * 3.6 + L2_ADDITIONAL_GAS
+        : PROCESS_PROPOSAL_GAS_LIMIT_ADDITION * GAS_BUFFER_MULTIPLIER + L2_ADDITIONAL_GAS
     );
   }
   return BigInt(
     Number(actionGasEstimate) > 0
       ? Number(actionGasEstimate) + PROCESS_PROPOSAL_GAS_LIMIT_ADDITION
-      : PROCESS_PROPOSAL_GAS_LIMIT_ADDITION * 3.6
+      : PROCESS_PROPOSAL_GAS_LIMIT_ADDITION * GAS_BUFFER_MULTIPLIER
   );
 };
 

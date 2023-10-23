76acc84e03dab74980af4d5696a927f6df62442c
dekanbro <dekanbrown@odyssy.io>
2023-10-23 12:02:11 -0600

    add dialog for larger textarea

--- libs/moloch-v3-fields/src/fields/MarkdownField.tsx
+++ libs/moloch-v3-fields/src/fields/MarkdownField.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
-import { Buildable, Button, Field, Label, WrappedTextArea } from '@daohaus/ui';
+import {
+  Buildable,
+  Button,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  Field,
+  Label,
+  WrappedTextArea,
+} from '@daohaus/ui';
 import { useFormContext } from 'react-hook-form';
 import { BiPencil } from 'react-icons/bi';
-import { MdPreview } from 'react-icons/md';
+import { MdFullscreen, MdFullscreenExit, MdPreview } from 'react-icons/md';
 
 import ReactMarkdown from 'react-markdown';
 
@@ -23,6 +32,21 @@ const MarkDownContainer = styled.div`
   margin-bottom: 5rem;
   border-radius: 5px;
   background-color: hsl(228, 43.3%, 17.5%);
+  font-size: 1.5rem;
+  font-family: inherit;
+`;
+
+const DialogMarkDownContainer = styled.div`
+  height: 50rem;
+  max-width: 100rem;
+  min-width: 50rem;
+  overflow-y: scroll;
+  padding: 10px;
+  margin-bottom: 5rem;
+  border-radius: 5px;
+  background-color: hsl(228, 43.3%, 17.5%);
+  font-size: 1.5rem;
+  font-family: inherit;
 `;
 
 const LabelContainer = styled(Label)`
@@ -38,12 +62,25 @@ const LabelContainer = styled(Label)`
   }
 `;
 
+const DialogWrappedTextArea = styled(WrappedTextArea)`
+  height: 50rem;
+  max-width: 100rem;
+  min-width: 70rem;
+`;
+
+const DialogButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  margin-bottom: -2rem;
+`;
+
 export const MarkdownField = (props: Buildable<Field>) => {
   const { watch } = useFormContext();
-  console.log('watch', watch(props.id));
   const value = watch(props.id);
   // use state for edit or preview view
   const [edit, setEdit] = useState(true);
+  const [toggleFullscreen, setToggleFullscreen] = useState(false);
 
   return (
     <>
@@ -64,8 +101,53 @@ export const MarkdownField = (props: Buildable<Field>) => {
         >
           <MdPreview />
         </Button>
+
+        <Dialog open={toggleFullscreen} onOpenChange={setToggleFullscreen}>
+          <DialogTrigger asChild>
+            <Button
+              color="secondary"
+              variant={toggleFullscreen ? 'outline' : 'solid'}
+              size="sm"
+            >
+              {toggleFullscreen ? <MdFullscreen /> : <MdFullscreenExit />}
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent title="Add Safe">
+            <DialogButtonWrapper>
+              <Button
+                color="secondary"
+                variant={!edit ? 'outline' : 'solid'}
+                onClick={() => setEdit(true)}
+                size="sm"
+              >
+                <BiPencil />
+              </Button>
+              <Button
+                color="secondary"
+                variant={edit ? 'outline' : 'solid'}
+                onClick={() => setEdit(false)}
+                size="sm"
+              >
+                <MdPreview />
+              </Button>
+            </DialogButtonWrapper>
+            {edit ? (
+              <DialogWrappedTextArea {...props} />
+            ) : (
+              <>
+                <LabelContainer>
+                  <Label>Preview</Label>
+                </LabelContainer>
+                <DialogMarkDownContainer>
+                  <ReactMarkdown>{value}</ReactMarkdown>
+                </DialogMarkDownContainer>
+              </>
+            )}
+          </DialogContent>
+        </Dialog>
       </TabsContainer>
-      {edit ? (
+      {edit && !toggleFullscreen ? (
         <WrappedTextArea {...props} />
       ) : (
         <>

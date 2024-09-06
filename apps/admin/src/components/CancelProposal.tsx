4666e052e7409cc759552abd3fc90144ff18eb5b
skuhlmann <samkuhlmann@gmail.com>
2024-09-06 11:50:10 -0600

    tooltip copy update

--- apps/admin/src/components/CancelProposal.tsx
+++ apps/admin/src/components/CancelProposal.tsx
@@ -98,7 +98,7 @@ export const CancelProposal = ({
 
     return isProposer || sponsorBelowThreshold || isGovernorShaman
       ? true
-      : `Proposal can only be cancelled by the proposer, by a governance shaman or if the sponsor's voting token balance has fallen below the sponsor threshold`;
+      : `Proposal can only be cancelled by the sponsor, by a governance shaman or by anyone if the sponsor's voting token balance has fallen below the sponsor threshold`;
   }, [proposal, address, dao]);
 
   if (proposal.status !== PROPOSAL_STATUS.voting) {
